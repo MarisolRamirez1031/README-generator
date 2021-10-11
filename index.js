810df254f380e2f8d96ec2e9e25daf85ba2ccf25
Marisol Bennett <marisol_ramirez21@live.com>
2021-10-11 00:10:47 -0500

    capturing user input and printing dynamic output

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 console.log('Hello Node, README!')
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
@@ -12,3 +16,18 @@ function init() {}
 
 // Function call to initialize app
 init(); 
+
+const printProfileData = (profileDataArr) => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('============')
+
+    // Is the same as this..
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem);
+    });
+};
+printProfileData(profileDataArgs);
