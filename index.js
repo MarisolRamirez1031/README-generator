4152485621ce8a1e1593fd681ba22cf68a4aaf79
Marisol Bennett <marisol_ramirez21@live.com>
2021-10-12 18:04:16 -0500

    added generatemarkdown const

--- index.js
+++ index.js
@@ -8,10 +8,88 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
-const generateMarkdown = require('./Develop/utils/generateMarkdown')
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+const generate = require('./Develop/utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your Project?',
+            validate: titleInput => {
+                if (titleInput) {
+                return true;
+                } else {
+                console.log('Please enter your project Title!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'What is your Project about?',
+            validate: aboutInput => {
+                if (aboutInput) {
+                return true;
+                } else {
+                console.log('Please enter your project description!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contents',
+            message: 'Will there be a Table of Contents?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?',
+            validate: installationInput => {
+                if (installationInput) {
+                return true;
+                } else {
+                console.log('You need to enter installation directions for your project!');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the usage instructions for your Project?',
+            validate: usageInput => {
+                if (usageInput) {
+                return true;
+                } else {
+                console.log('You need to enter a project GitHub link!');
+                return false;
+                }
+            }
+            
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Were there any contributing collaborators or third party assets used for this Project?',
+            default: false
+            
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license would you like to use for this Project?',
+            choices: ['MIT License', 'Apache 2.0 License', 'Mozilla Public License 2.0', 'Rails', 'Unlicensed']
+            
+        },
+        {
+
+    }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
